e3d34e534f066561b31cbc69e01fa6eea11b7f08
Otis Singfield <singfieldotis@gmail.com>
2025-04-23 04:01:52 -0400

    add arguments to bookbot

--- .history/stats_20250320203524.py
+++
@@ -1,3 +0,0 @@
-def num_of_words(string):
-    list = string.split()
-    return len(list)
--- .history/stats_20250320212042.py
+++
@@ -1,7 +0,0 @@
-def num_of_words(string):
-    list = string.split()
-    return len(list)
-
-
-def letter_counter(string):
-    low_string = string.lower()
